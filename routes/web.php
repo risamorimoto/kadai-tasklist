9e931e49781523af71414706c610314a9647b83c
risa.morimoto <risa.morimoto@rakuten.com>
2018-06-15 03:16:23 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
+// user registration
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
